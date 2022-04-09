5736cadb2ea930740f6b2b34726535ccc532b798
Zanetaa <91408269+Zanetaa@users.noreply.github.com>
2022-04-09 19:03:27 +0200

    pokus 3

--- kviz.js
+++ kviz.js
@@ -4,7 +4,10 @@ const otazky = [{
     cisloOtazky: 1,
     otazka: "Jaká je Žanety oblíbená pohádková princezna?",
     obrazek: "obrazky/snehurka.jpg",
-    moznosti: ['Sněhurka', 'Popelka', 'Malá mořská víla Ariel'],
+    /*moznosti: ['Sněhurka', 'Popelka', 'Malá mořská víla Ariel'],*/
+    odpoved1:'Sněhurka',
+    odpoved2:'Popelka',
+    odpoved3:'Malá mořská víla Ariel',
     spravne: 'Sněhurka'
     },
 
@@ -12,21 +15,30 @@ const otazky = [{
     cisloOtazky: 2,
     otazka: "Jaká je Žanety nejoblíbenější ovoce?",
     obrazek: "obrazky/ovoce.jpg",
-    moznosti: ['Jahody', 'Třešně', 'Meloun'],
+    /*moznosti: ['Jahody', 'Třešně', 'Meloun'],*/
+    odpoved1:'Jahody',
+    odpoved2:'Třešně',
+    odpoved3:'Meloun',
     spravne: 'Třešně'
     },
 
     {cisloOtazky: 3,
     otazka: "Která evrpská země má největší spotřebu piva na hlavu?",
     obrazek: "obrazky/pivo.jpg",
-    moznosti: ['Česká republika', 'Belgie', 'Německo'],
+    /*moznosti: ['Česká republika', 'Belgie', 'Německo'],*/
+    odpoved1:'Česká republika',
+    odpoved2:'Belgie',
+    odpoved3:'Německo',
     spravne: 'Česká republika'
     },
 
     {cisloOtazky: 4,
     otazka: "Co je ikonická hračka z 80. let?",
     obrazek: "obrazky/moncicak.jpg",
-    moznosti: ['Kočičák', 'Mončičák', 'Opičák'],
+    /*moznosti: ['Kočičák', 'Mončičák', 'Opičák'],*/
+    odpoved1:'Kočičák',
+    odpoved2:'Mončičák',
+    odpoved3:'Opičák',
     spravne: 'Mončičák'
     },
 
@@ -46,23 +58,28 @@ let obrazek = document.querySelector('#obrazek')
 let moznosti = document.querySelector('#moznosti')
 
 let odpovedi = document.querySelector('#odpovedi');
-let odpoved1 = document.createElement('li');
+let odpoved1 = document.createElement('li'); 
 let odpoved2 = document.createElement('li');
-let odpoved3 = document.createElement('li');
+let odpoved3 = document.createElement('li'); 
+
+odpovedi.appendChild(odpoved1);
+odpovedi.appendChild(odpoved2);
+odpovedi.appendChild(odpoved3);
+
 
 let vysledek = document.querySelector('.vysledek')
 
 
-let seznamOdpovedi = document.createElement('li');
+/* let seznamOdpovedi = document.createElement('li');
 
 let seznam = document.querySelector('ul');
-seznam.appendChild(seznamOdpovedi);
+seznam.appendChild(seznamOdpovedi); */
 
 
-
- for (let i = 0; i < 10 ; i=i+1) {
+//určitě budu používat " cyklus for"
+ /* for (let i = 0; i < 10 ; i=i+1) {
      console.log(i)
- }
+ } */
 
 
 // Tato funkce se postará o vygenerování otázky
