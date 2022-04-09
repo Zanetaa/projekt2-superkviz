e6dec9a28ebf6d750f42827662158025982c5eac
Zanetaa <91408269+Zanetaa@users.noreply.github.com>
2022-04-09 18:32:01 +0200

    Step1

--- kviz.js
+++ kviz.js
@@ -5,34 +5,50 @@ const otazky = [{
     otazka: "Jaká je Žanety oblíbená pohádková princezna?",
     obrazek: "obrazky/snehurka.jpg",
     moznosti: ['Sněhurka', 'Popelka', 'Malá mořská víla Ariel'],
-    spravne: "Sněhurka"
-},
+    spravne: 'Sněhurka'
+    },
 
     {   
     cisloOtazky: 2,
     otazka: "Jaká je Žanety nejoblíbenější ovoce?",
     obrazek: "obrazky/ovoce.jpg",
     moznosti: ['Jahody', 'Třešně', 'Meloun'],
-    spravne: "Třešně"
+    spravne: 'Třešně'
     },
 
     {cisloOtazky: 3,
     otazka: "Která evrpská země má největší spotřebu piva na hlavu?",
     obrazek: "obrazky/pivo.jpg",
     moznosti: ['Česká republika', 'Belgie', 'Německo'],
-    spravne: "Česká republika"
+    spravne: 'Česká republika'
     },
 
     {cisloOtazky: 4,
     otazka: "Co je ikonická hračka z 80. let?",
     obrazek: "obrazky/moncicak.jpg",
     moznosti: ['Kočičák', 'Mončičák', 'Opičák'],
-    spravne: "Mončičák"
+    spravne: 'Mončičák'
     },
 
 ];
 
 // Dále budeš potřebovat další proměnné - jaké?
+
+let kviz = document.querySelector('.kviz')
+let poradi = document.querySelector('#poradi')
+let otazka = document.querySelector('#otazka')
+
+let obsah = document.querySelector('.obsah')
+let foto = document.querySelector('.foto')
+
+let obrazek = document.querySelector('#obrazek')
+
+let moznosti = document.querySelector('#moznosti')
+let odpovedi = document.querySelector('#odpovedi')
+
+let vysledek = document.querySelector('.vysledek')
+
+let i =0;
 
 
 // Tato funkce se postará o vygenerování otázky
