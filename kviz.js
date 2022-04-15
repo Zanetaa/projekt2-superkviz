53c262a47d08001930b950d1209177adbfc99f9b
Zanetaa <91408269+Zanetaa@users.noreply.github.com>
2022-04-15 11:03:19 +0200

    Přidání odpovědí + čištění messu z minula

--- kviz.js
+++ kviz.js
@@ -1,45 +1,45 @@
 // Nejdříve si vytvoř objekt, který bude držet tvoje super otázky :-)
 
-const otazky = [{
+let otazky = [{
     cisloOtazky: 1,
     otazka: "Jaká je Žanety oblíbená pohádková princezna?",
     obrazek: "obrazky/snehurka.jpg",
-    moznostiOdpovedi: ['Sněhurka', 'Popelka', 'Malá mořská víla Ariel'],
-    /*moznost1:'Sněhurka',
-    moznost2:'Popelka',
-    moznost3:'Malá mořská víla Ariel',*/
-    spravne: 'Sněhurka' 
+    poleOdpovedi: ['Sněhurka', 'Popelka', 'Malá mořská víla Ariel'],
+    /*moznost[0]:'Sněhurka',
+    moznost[1]:'Popelka',
+    moznost[2]:'Malá mořská víla Ariel',*/
+    spravne: 0,
     },
 
     {   
     cisloOtazky: 2,
     otazka: "Jaká je Žanety nejoblíbenější ovoce?",
     obrazek: "obrazky/ovoce.jpg",
-    moznostiOdpovedi: ['Jahody', 'Třešně', 'Meloun'],
+    poleOdpovedi: ['Jahody', 'Třešně', 'Meloun'],
     /*moznost1:'Jahody',
     moznost2:'Třešně',
     moznost3:'Meloun',*/
-    spravne: 'Třešně'
+    spravne: 1,
     },
 
     {cisloOtazky: 3,
     otazka: "Která evrpská země má největší spotřebu piva na hlavu?",
     obrazek: "obrazky/pivo.jpg",
-    moznostiOdpovedi: ['Česká republika', 'Belgie', 'Německo'],
+    poleOdpovedi: ['Česká republika', 'Belgie', 'Německo'],
     /*moznost1:'Česká republika',
     moznost2:'Belgie',
     moznost3:'Německo',*/
-    spravne: 'Česká republika'
+    spravne: 0,
     },
 
     {cisloOtazky: 4,
     otazka: "Co je ikonická hračka z 80. let?",
     obrazek: "obrazky/moncicak.jpg",
-    moznostiOdpovedi: ['Kočičák', 'Mončičák', 'Opičák'],
-    /*moznost1:'Kočičák',
-    moznosz2:'Mončičák',
-    moznost3:'Opičák',*/
-    spravne: 'Mončičák'
+    poleOdpovedi: ['Kočičák', 'Mončičák', 'Opičák'],
+    /*moznost[0]:'Kočičák',
+    moznosz[1]:'Mončičák',
+    moznost[2]:'Opičák',*/
+    spravne: 1,
     },
 
 ];
@@ -49,44 +49,15 @@ const otazky = [{
 let kviz = document.querySelector('.kviz')
 let poradi = document.querySelector('#poradi')
 let otazka = document.querySelector('#otazka')
-
 let obsah = document.querySelector('.obsah')
 let foto = document.querySelector('.foto')
-
 let obrazek = document.querySelector('#obrazek')
-
-/*let moznosti = document.querySelector('#moznosti')*/
-
-//Z hodiny:
-/* let seznamOdpovedi = document.createElement('li');
-
-let seznam = document.querySelector('ul');
-seznam.appendChild(seznamOdpovedi); */
-
-/*let odpovedi = document.querySelector('#odpovedi');*/
-/*let moznosti = document.createElement('li');*/
-
-/* let moznost = document.createElement('li'); 
-
-odpovedi.appendChild(moznost); */
-
-/*let odpovedi = document.createElement('ul');
-let moznost = document.createElement('li');
-moznost.innerHTML = otazky[zobrazenaOtazka].moznostiOdpovedi[a];
-odpovedi.appendChild(moznost);
-document.querySelector('moznosti').appendchild(odpovedi); */
-
-
+let moznosti = document.querySelector('#moznosti')
+let odpovedi = document.querySelector('#odpovedi');
 let vysledek = document.querySelector('.vysledek');
 
 // Tato funkce se postará o vygenerování otázky
 // Zavoláme ji jednou na začátku a poté vždy po odpovězení
-
-//Z hodiny:
-//" cyklus for"
- /* for (let i = 0; i < 10 ; i=i+1) {
-     console.log(i)
- } */
 
  let zobrazenaOtazka =0;
 
@@ -95,32 +66,20 @@ let vysledek = document.querySelector('.vysledek');
     obrazek.src = otazky[zobrazenaOtazka].obrazek;
     otazka.innerHTML = otazky[zobrazenaOtazka].otazka;
 
-    /*moznost.innerHTML = otazky[zobrazenaOtazka].moznostiOdpovedi[i];*/
+    for (let i=0; i < otazky[zobrazenaOtazka].poleOdpovedi.length; i=i+1) {
 
-    /* let moznost1 = document.createElement('li'); 
-    let moznost2 = document.createElement('li');
-    let moznost3 = document.createElement('li'); 
+    let volba = document.createElement('li');
+    volba.dataset.poleOdpovedi = i;
+    volba.innerHTML = otazky[zobrazenaOtazka].poleOdpovedi[i];
+    odpovedi.appendChild(volba);
 
-    odpovedi.appendChild(moznost1);
-    odpovedi.appendChild(moznost2);
-    odpovedi.appendChild(moznost3); */
+    //hází mí to ty odpovědi dvakrát a nevím proč :( )
 
-    /*moznost1.innerHTML = otazky[zobrazenaOtazka].moznost1;
-    moznost2.innerHTML = otazky[zobrazenaOtazka].moznost2;
-    moznost3.innerHTML = otazky[zobrazenaOtazka].moznost3;*/
-
-    //nedaří se mi připojit ten set možností..otázka se mi načte jen když mám nahoře vypasné otázky bod po bodu :(..raději bych měla vypasné jako set
-    for (let i=0; i < otazky[zobrazenaOtazka]; i=i+1) {
-
-    let odpovedi = document.querySelector('#odpovedi')
-    let moznost = document.createElement('li');
-
-    odpovedi.appendChild(moznost);
-
-    moznost.innerHTML = otazky[zobrazenaOtazka].moznostiOdpovedi[i];
      }
 
  }
+
+
 
  zobrazOtazku();
 
